bbf7b28cde2f880ed9406bd3461b48da3cc80930
Dmitriy <sova123409@gmail.com>
2024-01-21 12:05:26 +0300

    Upgrade all dependencies and remove front

--- apps/front/components/breed-group/form/config.ts
+++
@@ -1,7 +0,0 @@
-import type { UpdateBreedGroupDto } from '@hb/back/src/clients/cruds/breed-group/dto';
-
-export const defaultForm = () =>
-  ({
-    name: '',
-    fci: null,
-  } as UpdateBreedGroupDto);

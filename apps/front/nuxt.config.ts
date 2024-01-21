0bc644935b5a38a99e5a9fbced873383d244421e
Dmitriy <sova123409@gmail.com>
2024-01-21 13:31:10 +0300

    Add raw locales

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@vite-pwa/nuxt',
+    '@nuxtjs/i18n',
+  ],
+  pwa: {
+
+  },
+  i18n: {
+
+  },
 })
